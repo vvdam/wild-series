bba443c2a0056138bdc2cfb840d9202d793897cd
vvdam <maaalouladam@gmail.com>
2024-06-03 11:37:12 +0200

    t

--- server/app/routers/api/programs/router.js
+++ server/app/routers/api/programs/router.js
@@ -3,16 +3,21 @@ const express = require("express");
 const router = express.Router();
 
 /* ************************************************************************* */
-// Import And Use Routers Here
+// Define Your API Routes Here
 /* ************************************************************************* */
 
-const itemsRouter = require("./items/router");
+// Import item-related actions
+const { browse, read, add } = require("../../../controllers/programActions");
 
-router.use("/items", itemsRouter);
+// Route to get a list of items
+router.get("/", browse);
 
-const programRouter = require("./programs/router");
+// Route to get a specific item by ID
+router.get("/:id", read);
 
-router.use("/programs", programRouter);
+// Route to add a new item
+router.post("/", add);
+
 /* ************************************************************************* */
 
 module.exports = router;
